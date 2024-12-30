b2433fd0da17132b1623f76d2269ca47a8b55773
AzureAD\VishalKhalate <vkhalate@annexcloud.com>
2024-12-30 15:40:13 +0530

    Name change 01

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angulartotorial'),
+      dir: require('path').join(__dirname, './coverage/angulartutorial'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angulartotorial'`, () => {
+  it(`should have as title 'angulartutorial'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angulartotorial');
+    expect(app.title).toEqual('angulartutorial');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angulartotorial!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angulartutorial!');
   });
 });
